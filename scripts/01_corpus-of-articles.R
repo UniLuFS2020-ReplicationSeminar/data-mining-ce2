eb4702590cdd726f5729e0910255292940420f62
Ursulina Kölbener <ursulina@koelbener.ch>
2023-04-16 09:39:25 +0200

    add Jan

--- scripts/01_corpus-of-articles.R
+++ scripts/01_corpus-of-articles.R
@@ -1,6 +1,6 @@
 ###################################################################
 # Class Exercise 2
-# Sascha Eng & Ursulina Kölbener
+# Jan Bühlmann, Sascha Eng & Ursulina Kölbener
 ###################################################################
 
 # 1. install and load packages ----
@@ -14,3 +14,12 @@ library(tidyverse)
 # 2. save personal API key ----
 api_key <- rstudioapi::askForPassword()
 # make sure the key is not pushed to github
+
+# 3. GET requests to guardian.com ----
+base_url <- "https://content.guardianapis.com/search"
+searchterms <- "northern ireland protocol OR northern ireland deal OR windsor framework"
+
+resp <- httr::GET(base_url, 
+                  query = list(key = api_key, q = searchterms))
+
+http_status(resp)
